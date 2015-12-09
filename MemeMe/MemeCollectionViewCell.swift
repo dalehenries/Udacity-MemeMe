1a2a2384d8259d37334d8e6aca0afd8299e8fe81
Dale Henries <dhenries@samaritan.org>
2015-12-09 12:16:50 -0500

    Complete. Before clean up for submission.

--- MemeMe/MemeCollectionViewCell.swift
+++ MemeMe/MemeCollectionViewCell.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class MemeCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
     func populateCell(meme: Meme) {
-        self.backgroundView = UIImageView(image: meme.memedImage)
-        
+        //self.backgroundView = UIImageView(image: meme.memedImage)
+        imageView.image = meme.memedImage
     }
 
 }
--- MemeMe/SentMemesCollectionViewController.swift
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -11,14 +11,35 @@ import UIKit
 class SentMemesCollectionViewController: BaseSentMemesViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
     var memes: [Meme] {
         return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculateFlowLayout(view.frame.size)
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.collectionView.reloadData()
+    }
+    
+    // MARK: Custom Methods
+    func calculateFlowLayout(size: CGSize) {
+        let space: CGFloat = 3.0
+        // 3 items along the short side and 5 along the long side.
+        let dimension = size.width < size.height ? (size.width - (2 * space)) / 3.0 : (size.width - (4 * space)) / 5.0
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        // When the screen rotates, recalculate the flow layout
+        calculateFlowLayout(size)
     }
     
     // MARK: UICollectionViewDataSource Implementation
